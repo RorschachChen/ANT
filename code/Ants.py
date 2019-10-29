4b8b49b57c70c12a2238551d8e2d5cd5e37c562f
陈逸鸣 <chenyiming@chenyimingdeMacBook-Pro.local>
2019-10-29 10:13:27 +0800

    version2.0

--- code/Ants.py
+++ code/Ants.py
@@ -1,22 +1,28 @@
+from Ant import *
+from readin import *
+import numpy as np
+
 AntNum = 2000
 n = 10
-
-from Ant import *
-import numpy as np
-SR = ""
+q0 = 0.9
+q1 = 0.95
+alpha = 1
+beta = 2
+dirpath = '../dataset/test_data/dna_rat_10/'
+SR = readin()
 # phero shape [string_num， lengthOf1 string]
-phero = []
+phero = []# eta shape [string_num， lengthOf1 string]
 for i in range(n):
 	li = [1.0 for j in range(len(SR[i]))]
 	phero.append(li)
 # eta shape [string_num， lengthOf1 string]
-eta = []
-for i in range(n):
-	eta.append(li)
+eta = phero
 
-currentBest = ant_des(phero, eta)
+currentBest = ant_des(phero, eta, SR, q0, q1, alpha, beta)
 
 for i in range(AntNum):
-	solu = ant_des(phero, eta)
+	solu = ant_des(phero, eta, SR, q0, q1, alpha, beta)
 	if len(currentBest)<len(solu):
 		currentBest = solu
+
+print(len(currentBest))
--- code/readin.py
+++ code/readin.py
@@ -1,13 +1,22 @@
-path = '../dataset/test_data/dna_rat_10/dna_rat_10_BZ135269.fasta'
-with open(path, 'r+') as f:
-    seq={}
-    for line in f:
-        if line.startswith('>'):
-            name=line.replace('>','').split()[0]
-            seq[name]=''
-        else:
-            seq[name]+=line.replace('\n','').strip()
+import os
 
-print(len(seq['gi|23776216|gb|BZ135269.1|BZ135269']))
+def readin(dirpath='../dataset/test_data/dna_rat_10/'):
+    # path = '../dataset/test_data/dna_rat_10/dna_rat_10_BZ135269.fasta'
+    # dirpath = '../dataset/test_data/dna_rat_10/'
+    files= os.listdir(dirpath)
+    SR = ["" for i in range(len(files))]
+    print(files)
+    for i in range(len(files)):
+        with open(dirpath+files[i], 'r+') as f:
+            for line in f:
+                if line.startswith('>'):
+                    continue
+                else:
+                    SR[i]+=line.replace('\n','').strip()
 
-# print(len('AATTGGAATTCTCTCTTCCTGGTCTGTCTAAGATGGGGATAAATTGGTGTAAAAAAAAAAAAAAAAAAAA'))
+    # print(len(seq['gi|23776216|gb|BZ135269.1|BZ135269']))
+
+    # print(len('AATTGGAATTCTCTCTTCCTGGTCTGTCTAAGATGGGGATAAATTGGTGTAAAAAAAAAAAAAAAAAAAA'))
+    # for i in range(len(files)):
+    #   print(len(SR[i]))
+    return SR
